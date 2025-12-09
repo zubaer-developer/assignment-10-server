1d61b4a71db8d3c2a50e47b1dc0a0237b8856b5e
ZUBAER <zubaer.developer@gmail.com>
2025-12-09 18:39:05 +0600

    install express, cors, dotenv, mongodb and configure db connection

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let db;
+let usersCollection;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db(process.env.DB_NAME);
+    console.log("Database Connected Successfully");
+
+    usersCollection = db.collection("users");
+    console.log("Users Collection Ready");
+  } catch (error) {
+    console.log("Database Connection Failed:", error);
+  }
+}
+
+connectDB();
+
+// Add New User
+app.post("/users", async (req, res) => {
+  const user = req.body;
+  const result = await usersCollection.insertOne(user);
+  res.send(result);
+});
+
+app.get("/", (req, res) => {
+  res.send("PawMart Server is Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
